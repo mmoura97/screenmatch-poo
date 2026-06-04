e1ad8ad3ac36b10aac50064583d7fde962b28efa
mmoura97 <matheus.mourd30@gmail.com>
2026-06-03 21:08:17 -0300

    feat: adiciona classe Filme e metodos de avaliacao

---
+++ src/Filme.java
@@ -0,0 +1,23 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacao;
+    int duracaoEmMinutos;
+
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+}
